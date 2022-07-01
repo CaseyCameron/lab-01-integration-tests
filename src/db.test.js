410600e081bd59c086d6a13d4cf02522cea159fa
CaseyCameron <mcasey.cameron@gmail.com>
2022-07-01 10:17:58 -0700

    complete setup and teardown test

--- src/db.test.js
+++ src/db.test.js
@@ -1,35 +1,28 @@
-import { MongoClient } from 'mongodb';
 import { expect } from 'chai';
+import { afterEach } from 'mocha';
 import { getUserByUsername } from './db';
+import { getDatabaseData, setDatabaseData, resetDatabase } from './test-helpers';
 
 describe('getUserByUsername', () => {
-  it('gets the correct user from the db given a username', async () => {
-    const client = await MongoClient.connect(
-      `mongodb://localhost:27017/TEST_DB`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
-    );
-    const db = client.db('TEST_DB');
+  afterEach('reset the database', async () => {
+    await resetDatabase();
+  });
 
+  it('gets the correct user from the db given a username', async () => {
     const fakeData = [{
       id: '123',
       username: 'abc',
-      email: 'abc@gmail.com',
+      email: 'abc@gmail.com'
     },
     {
       id: '124',
       username: 'wrong',
       email: 'wrong@wrong.com'
-    }]
-    
-    await db.collection('users').insertMany(fakeData);
+    }];
+
+    await setDatabaseData('users', fakeData);
     const actual = await getUserByUsername('abc');
-    const finalDBState = await db.collection('users').find().toArray();
-    await db.dropDatabase();
-    
-    client.close();
+    const finalDBState = await getDatabaseData('users');
 
     const expected = {
       id: '123',
